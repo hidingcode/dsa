8928cd78e61167e41fe81913d29978cbc0f09fd1
hidingcode <74244077+hidingcode@users.noreply.github.com>
2022-08-31 18:49:57 +0800

    Add files via upload

---
+++ StackInterface.java
@@ -0,0 +1,10 @@
+package ADT;
+
+public interface StackInterface<T> {
+
+  public void push(T newEntry);
+  public T pop();
+  public T peek();
+  public boolean isEmpty();
+  public void clear();
+}
