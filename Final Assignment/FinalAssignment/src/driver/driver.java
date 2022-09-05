9b97cf7aaa915a07807d8744f88cfb191043e87b
hidingcode <yclim4635@gmail.com>
2022-09-05 22:38:17 +0800

    update kok ping

--- Final Assignment/FinalAssignment/src/driver/driver.java
+++ Final Assignment/FinalAssignment/src/driver/driver.java
@@ -1,35 +1,30 @@
 package driver;
-<<<<<<< HEAD
+
+import adt.SortedArrayListInterface;
+import adt.SortedArrayList;
+import java.util.Iterator;
 import adt.LinkedStack;
 import entity.Inventory;
 
 public class driver {
-    public static void main(String[] args) {
-=======
-import adt.SortedArrayListInterface;
-import adt.SortedArrayList;
-import entity.Inventory;
 
-import java.util.Iterator;
+        public static void main(String[] args) {
+            SortedArrayListInterface<Inventory> invList = new SortedArrayList<>();
+            // this.invCode = invCode;
+            // this.invName = invName;
+            // this.quantity = quantity;
+            // this.price = price;
+            invList.add(new Inventory("inv001"));
+            invList.add(new Inventory("inv002"));
+            invList.add(new Inventory("inv003"));
+            invList.add(new Inventory("inv004"));
+            invList.toString();
 
-public class driver {
-    public static void main(String[] args) {
-        SortedArrayListInterface<Inventory> invList = new SortedArrayList<>();
-        // this.invCode = invCode;
-        // this.invName = invName;
-        // this.quantity = quantity;
-        // this.price = price;
-        invList.add(new Inventory("inv001"));
-        invList.add(new Inventory("inv002"));
-        invList.add(new Inventory("inv003"));
-        invList.add(new Inventory("inv004"));
-        invList.toString();
-
-        Iterator<Inventory> invIterator = invList.getIterator();
-        while(invIterator.hasNext()){
-            Inventory inv = invIterator.next();
-            System.out.println(inv.getInvCode());
+            Iterator<Inventory> invIterator = invList.getIterator();
+            while(invIterator.hasNext()){
+                Inventory inv = invIterator.next();
+                System.out.println(inv.getInvCode());
         }
->>>>>>> 82f389d878f091e2ceb600b989cebddee9171aa7
+
     }
 }
