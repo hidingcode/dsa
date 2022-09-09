e83e7ae99986f8e4eabb698578f96bc47647231f
hidingcode <yclim4635@gmail.com>
2022-09-10 00:26:02 +0800

    Kitchen module done (if no combination)

--- Final Assignment/FinalAssignment/src/control/Kitchen.java
+++ Final Assignment/FinalAssignment/src/control/Kitchen.java
@@ -12,14 +12,14 @@ public class Kitchen {
     LinkedStackInterface<Chef> chefShift = new LinkedStack<Chef>();
     
     public void GetOrder(){
-        
+        UpdateChef();
     }
     
     public void UpdateInventory(){ 
         
     }
     
-    public void UI(){
+    public void MainUI(){
         System.out.println();
         System.out.println("Kitchen Module");
         System.out.println("1) Punch In\t2) Show Chef Shift\t3) Show Last Punch In\t4) Search");
@@ -28,13 +28,13 @@ public class Kitchen {
     }
 
     public void Main(){
-        UI();
+        MainUI();
         Scanner sc = new Scanner(System.in);
         String option = sc.next();
         switch(option){
             case "1":
                 // Allow chef to punch in
-                PunchInMenu();
+                PunchInUI();
                 break;
             case "2":
                 // Allow chef to check the chef shift
@@ -45,38 +45,41 @@ public class Kitchen {
                 ShowLastPunchIn();
                 break;
             case "4":
-                System.out.println(SearchChef(1).getChefName());
-                break; 
-            case "5":
-                break;       
+                SearchChefUI();
+                break;     
             default:
                 // Do nothing an end the program
         }
     }
-
-    public void PunchInMenu(){
+    // Punch In User Interface
+    public void PunchInUI(){
+        // Receive chef ID
         System.out.print("Please enter your ID: ");
         Scanner sc = new Scanner(System.in);
         String id = sc.next();
+        // Receive chef Name
         System.out.print("Please enter your Name: ");
         String name = sc.next();
         System.out.println();
         PunchIn(id, name);  
     }
 
+    // Get chefID and chefName and push the information into the stack
     public void PunchIn(String chefID, String chefName){ 
         Chef newChef = new Chef(chefID, chefName); 
         chefShift.push(newChef);
         System.out.println("Punch In Succesfully");
         Main();   
     }
-
+    // Show the Chef Shift
     public void ShowChefShift(){
+        // If the chefShift is empty print "No Chef Avaliable"
         if(chefShift.isEmpty())
         {
             System.out.println("No Chef Avaliable");
         }
         else{
+            // Loop through the stack with Iterator 
             Iterator<Chef> chefIterator = chefShift.getIterator();
             System.out.println();
             System.out.println("Chef Shift List");
@@ -115,6 +118,24 @@ public class Kitchen {
             // Remoce chef from the stack
             chefShift.pop();
         }
+    }
+
+    // Search Chef User Interface (Search the chef with the punchInSequence)
+    public void SearchChefUI(){
+        System.out.println("The 'n' punch in chef");
+        System.out.println();
+        System.out.println("n: ");
+        Scanner sc = new Scanner(System.in);
+        int punchInSequence = sc.nextInt();
+        // Get the result ChefID
+        String resultChefID = SearchChef(punchInSequence).getChefID();
+        // Get the result ChefName
+        String resultChefName = SearchChef(punchInSequence).getChefName();
+        System.out.println("The" + punchInSequence + "punch in chef");
+        System.out.println();
+        System.out.printf("%-10s %-15s\n","Chef ID","Chef Name");
+        System.out.printf("%-10s %-15s\n",resultChefID,resultChefName);  
+        Main();
     }
 
     public Chef SearchChef(int punchInSequence){
