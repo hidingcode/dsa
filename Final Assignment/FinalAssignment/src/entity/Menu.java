a1267cfc3c1282f7115eaa39efdadbbb9a38be15
Wei Yuan <adrianzzxx00@gmail.com>
2022-09-11 01:32:25 +0800

    ...

--- Final Assignment/FinalAssignment/src/entity/Menu.java
+++ Final Assignment/FinalAssignment/src/entity/Menu.java
@@ -1,5 +1,16 @@
 package entity;
 
 public class Menu {
-    
+    private int MenuID;
+    private String MenuName;
+    private String MenuDesc;
+    private int InvQuantity;
+    private float price;
+
+    Menu(int MenuID, String MenuName, String MenuDesc, String InvQuantity, float price){
+        this.MenuID = MenuID;
+        this.MenuName = MenuName;
+        this.InvQuantity = InvQuantity;
+        this.price = price;
+    }
 }
