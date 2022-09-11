4a14fcbf3c3c94a5441161ba1f15f94cf31d27ad
Wei Yuan <adrianzzxx00@gmail.com>
2022-09-11 18:21:12 +0800

    menudetail

--- Final Assignment/FinalAssignment/src/control/MenuDetail.java
+++ Final Assignment/FinalAssignment/src/control/MenuDetail.java
@@ -9,7 +9,15 @@ import java.util.Iterator;
 public class MenuDetail {
     ArrayListInterface<Menu> menuList = new ArrayList<Menu>();
 
-    public void main(){
+    public void MenuList(){
+        ArrayListInterface<Menu> menuList = new ArrayList<>();
+        Menu menu;
+        menuList.add(menu = new Menu("M001","Black pepper Steak","Steak with black pepper","Inv001",45));
+        menuList.add(menu = new Menu("M002","Black pepper Pork","Pork with black pepper","Inv002", 45));
+        menuList.add(menu = new Menu("M001","Black pepper Steak","Steak with black pepper","Inv001", 45));
+    }
+
+    public void Main(){
         Iterator<Menu> menuIterator = menuList.getIterator();
         System.out.println("\nMenu list: ");
         while (menuIterator.hasNext()){
