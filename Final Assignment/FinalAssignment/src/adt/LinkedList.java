bf6be2a1f7ecc74ba29c9be836e22949ec6293c8
Binkozaru <binmy2000@gmail.com>
2022-09-11 13:31:18 +0800

    migrate & add comment

--- Final Assignment/FinalAssignment/src/adt/LinkedList.java
+++ Final Assignment/FinalAssignment/src/adt/LinkedList.java
@@ -1,5 +1,5 @@
-package adt;
+// package adt;
 
-public class LinkedList<T> implements LinkedListInterface<T>{
+// public class LinkedList<T> implements LinkedListInterface<T>{
     
-}
+// }
--- Final Assignment/FinalAssignment/src/driver/driver.java
+++ Final Assignment/FinalAssignment/src/driver/driver.java
@@ -19,35 +19,37 @@ import java.util.Stack;
 
 import control.Kitchen;
 import control.AccountControl;
+import control.StockControl;
 
 public class driver {
     public static void main(String[] args) {
-        // SortedArrayListInterface<Inventory> invList = new SortedArrayList<>();   
-        // Inventory inventory;
-        // invList.add(inventory = new Inventory("Inv001", "Steak", 25, 100));
-        // invList.add(inventory = new Inventory("Inv002", "Pork", 25, 80));
-        // invList.add(inventory = new Inventory("Inv003", "Lamb", 25, 90));
-        // invList.add(inventory = new Inventory("Inv004", "Fish", 25, 70));
-        // invList.add(inventory = new Inventory("Inv005", "Bread", 25, 50));
-        // invList.add(inventory = new Inventory("Inv006", "Sugar", 25, 30));
+        SortedArrayListInterface<Inventory> invList = new SortedArrayList<>();   
+        Inventory inventory;
+        StockControl stocks = new StockControl();
+        invList.add(inventory = new Inventory("Inv001", "Steak", 25, 100));
+        invList.add(inventory = new Inventory("Inv002", "Pork", 25, 80));
+        invList.add(inventory = new Inventory("Inv003", "Lamb", 25, 90));
+        invList.add(inventory = new Inventory("Inv004", "Fish", 25, 70));
+        invList.add(inventory = new Inventory("Inv005", "Bread", 25, 50));
+        invList.add(inventory = new Inventory("Inv006", "Sugar", 25, 30));
 
 
-        // Inventory inventory2 = new Inventory("Inv004", "Fish", 25, 100);
-        // inventory.outStock(invList, inventory2, true, 25);
-        // inventory.main(invList, inventory);
+        Inventory inventory2 = new Inventory("Inv004", "Fish", 25, 100);
+        stocks.outStock(invList, inventory2, stocks, true, 25);
+        stocks.main(invList, inventory, stocks);
         
-        ArrayListInterface<Customer> cusList = new ArrayList<>();
-        ArrayListInterface<Staff> staffList = new ArrayList<>();
-        Staff staff;
-        Customer customer;
-        staffList.add(staff = new Staff(1101, "waimin", "waimin", 2, 201));
-        staffList.add(staff = new Staff(1102, "quack", "quack", 2, 202));
-        cusList.add(customer = new Customer(1001, "nengfu14", "nengfu14", 1, 1234567890, "GAY"));
-        cusList.add(customer = new Customer(1002, "YKP", "YKP", 1, 1234567890, "GAY2"));
-        cusList.add(customer = new Customer(1003, "Pikachu", "Pikachu", 1, 1234567890, "GAY3"));
-        cusList.add(customer = new Customer(1004, "YWY", "YWY", 1, 1234567890, "GAY4"));
-        AccountControl accControl = new AccountControl();
-        accControl.main(staff, customer, staffList, cusList);
+        // ArrayListInterface<Customer> cusList = new ArrayList<>();
+        // ArrayListInterface<Staff> staffList = new ArrayList<>();
+        // Staff staff; 
+        // Customer customer;
+        // staffList.add(staff = new Staff(1101, "waimin", "waimin", 2, 201));
+        // staffList.add(staff = new Staff(1102, "quack", "quack", 2, 202));
+        // cusList.add(customer = new Customer(1001, "nengfu14", "nengfu14", 1, 1234567890, "GAY"));
+        // cusList.add(customer = new Customer(1002, "YKP", "YKP", 1, 1234567890, "GAY2"));
+        // cusList.add(customer = new Customer(1003, "Pikachu", "Pikachu", 1, 1234567890, "GAY3"));
+        // cusList.add(customer = new Customer(1004, "YWY", "YWY", 1, 1234567890, "GAY4"));
+        // AccountControl accControl = new AccountControl();
+        // accControl.main(staff, customer, staffList, cusList);
 
         
         //  
