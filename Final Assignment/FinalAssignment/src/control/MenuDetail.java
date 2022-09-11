6e5907629f9a476b99ff27da109c06bd25a30cb4
Wei Yuan <adrianzzxx00@gmail.com>
2022-09-11 17:35:15 +0800

    small change

---
+++ Final Assignment/FinalAssignment/src/control/MenuDetail.java
@@ -0,0 +1,13 @@
+package control;
+
+import java.util.Iterator;
+import java.util.Scanner;
+import entity.Menu;
+
+public class MenuDetail {
+    
+    public void main(){
+        System.out.println("\nMenu list: ");
+        
+    }
+}
