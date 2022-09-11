cd9348241d1ad4edb889af94a120bcb29dbcb980
Wei Yuan <adrianzzxx00@gmail.com>
2022-09-11 18:02:18 +0800

    small change

--- Final Assignment/FinalAssignment/src/control/MenuDetail.java
+++ Final Assignment/FinalAssignment/src/control/MenuDetail.java
@@ -1,13 +1,20 @@
 package control;
 
-import java.util.Iterator;
-import java.util.Scanner;
+import adt.ArrayList;
+import adt.ArrayListInterface;
 import entity.Menu;
 
+import java.util.Iterator;
+
 public class MenuDetail {
-    
+    ArrayListInterface<Menu> menuList = new ArrayList<Menu>();
+
     public void main(){
+        Iterator<Menu> menuIterator = menuList.getIterator();
         System.out.println("\nMenu list: ");
-        
+        while (menuIterator.hasNext()){
+            Menu menu = menuIterator.next();
+            System.out.printf(menu.getMenuID(), menu.getMenuName(), menu.getMenuDesc(), menu.getMenuPrice());
+        }
     }
 }
